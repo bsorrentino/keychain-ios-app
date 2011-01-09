360481a30979ca3963afa8bea7846045dbc1acdf
softphone <bartolomeo.sorrentino@gmail.com>
2011-01-09 15:38:20 +0100

    project for test UIXML framework

---
+++ TableViewDataEntry/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TableViewDataEntry
+//
+//  Created by Fabrizio Guglielmino on 31/08/10.
+//  Copyright Infit S.r.l 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
