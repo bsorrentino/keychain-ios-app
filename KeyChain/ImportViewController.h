fb86458c3569adffdd959e2d8401001022096182
softphone <bartolomeo.sorrentino@gmail.com>
2011-10-09 12:03:14 +0200

    add import feature

---
+++ KeyChain/ImportViewController.h
@@ -0,0 +1,16 @@
+//
+//  ImportViewController.h
+//  KeyChain
+//
+//  Created by softphone on 09/10/11.
+//  Copyright 2011 SOFTPHONE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ImportViewController : UITableViewController {
+    
+}
+
+@end
