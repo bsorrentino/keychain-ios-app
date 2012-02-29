702f6ee5ce77a1fdee652568043bc759a3e1e3df
bsorrentino <bartolomeo.sorrentino@gmail.com>
2012-02-29 16:33:29 +0100

    add drag & drop project

---
+++ DragDrop/DragDropTests/DragDropTests.h
@@ -0,0 +1,13 @@
+//
+//  DragDropTests.h
+//  DragDropTests
+//
+//  Created by softphone on 09/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DragDropTests : SenTestCase
+
+@end
