1f1976276447edf170911f7ca5cdef4b72c8906c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2012-09-20 19:39:07 +0200

    add migration splash screen

---
+++ KeyChain/YISplashScreenAnimation.h
@@ -0,0 +1,18 @@
+//
+//  YISplashScreenAnimation.h
+//  YISplashScreen
+//
+//  Created by Yasuhiro Inami on 12/06/15.
+//  Copyright (c) 2012年 Yasuhiro Inami. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "YISplashScreen.h"
+
+
+@interface YISplashScreenAnimation : NSObject
+
++ (YISplashScreenAnimationBlock)pageCurlAnimation;
++ (YISplashScreenAnimationBlock)cubeAnimation;
+
+@end
