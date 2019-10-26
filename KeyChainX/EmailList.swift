b93d9f9888c72172cab64d62dc8e675ee026c268
bsorrentino <bartolomeo.sorrentino@gmail.com>
2019-10-26 22:19:25 +0200

    add mail form

--- KeyChainX/EmailList.swift
+++ KeyChainX/EmailList.swift
@@ -7,11 +7,36 @@
 //
 
 import SwiftUI
+import FieldValidatorLibrary
 
-
+struct EmailForm : View {
+    
+    @State var mailValid = FieldChecker()
+    @State var result:String = ""
+    
+    var body: some View {
+        
+       VStack(alignment: .leading) {
+            //Text("mail")
+            TextFieldWithValidator( title: "insert email", value: $result, checker:$mailValid ) { v in
+                   
+                   if( v.isEmpty ) {
+                       return "mail cannot be empty"
+                   }
+                   
+                   return nil
+            }
+        }
+       .padding(10.0)
+    }
+}
 
 struct EmailList: View {
+    @State private var showModal = false
+    
     @Binding var value:String
+    
+    @Environment(\.managedObjectContext) var context
     
     @FetchRequest( entity: MailEntity.entity(),
                    sortDescriptors: [ NSSortDescriptor(keyPath: \MailEntity.name, ascending: true)]
@@ -20,19 +45,47 @@ struct EmailList: View {
     
     var body: some View {
         
-        List(mails, id: \MailEntity.id) { (mail:MailEntity) in
-            Text( mail.name ?? "unknown")
-        }
-        .padding(.all)
-        .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
+        NavigationView {
+            
+            List {
+                ForEach( mails, id: \MailEntity.id ) { (mail:MailEntity) in
+                    Text( mail.name ?? "unknown")
+                }
+                .onDelete( perform: delete)
+            }
+             .navigationBarTitle("email")
+             .navigationBarItems(
+                 leading: Button( action: add, label: { Text("Add") } ),
+                 trailing: EditButton())
+         }.sheet(isPresented: $showModal, onDismiss: {
+             print(self.showModal)
+         }) {
+             EmailForm()
+         }
     
+    }
+    
+    func delete( at offsets: IndexSet ) {
+        if let first = offsets.first {
+            let selectMail = mails[ first ]
+            
+            context.delete(selectMail)
+        }
+    }
+    
+    func add() {
+        self.showModal = true
     }
 }
 
 #if DEBUG
 struct EmailList_Previews: PreviewProvider {
     static var previews: some View {
-        EmailList( value:.constant(""))
+        // https://stackoverflow.com/questions/57700304/previewing-contentview-with-coredata
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        return EmailList( value:.constant("")).environment(\.managedObjectContext, context)
     }
 }
 #endif
