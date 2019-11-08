587c3db1fff83514207dbe625ca1e4322a3a8eb7
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2019-11-08 20:43:24 +0100

    update data model to be compatible with the previous one

---
+++ KeyChainX/KeyEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  KeyEntity+CoreDataClass.swift
+//  
+//
+//  Created by Bartolomeo Sorrentino on 08/11/2019.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(KeyEntity)
+public class KeyEntity: NSManagedObject {
+
+}
