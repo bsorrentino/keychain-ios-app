9133690ed19606f53e42d434bf765e5ee9d8f0c5
bsorrentino <bartolomeo.sorrentino@gmail.com>
2020-01-02 19:57:05 +0100

    fix #25 start Observing ManagedObjectContextObjectsDidChangeEvent to handle delete ManagedObject

--- KeyChainX/App+CoreData.swift
+++ KeyChainX/App+CoreData.swift
@@ -27,54 +27,6 @@ enum SavingError :Error {
     case BundleIdentifierUndefined
 }
 
-/**
-    Fetch Single Value
- */
-func fetchSingle( _ context:NSManagedObjectContext, entity:NSEntityDescription, predicateFormat:String, key:String  ) throws -> Any {
-
-    let request = NSFetchRequest<NSFetchRequestResult>()
-    request.entity =  entity
-    request.predicate = NSPredicate( format: predicateFormat, key)
-    let fetchResult = try context.fetch( request )
-    
-    if( fetchResult.count == 0 ) {
-        throw SavingError.KeyDoesNotExist(id: key)
-        
-    }
-    if( fetchResult.count > 1 ) {
-        throw SavingError.DuplicateKey(id: key)
-        
-    }
-    return fetchResult[0]
-}
-
-//
-// @see https://www.avanderlee.com/swift/nsbatchdeleterequest-core-data/
-//
-func deleteAll( into context:NSManagedObjectContext ) throws {
-    
-    let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: KeyEntity.fetchRequest())
-    
-    batchDeleteRequest.resultType = .resultTypeObjectIDs
-
-    let result = try context.execute(batchDeleteRequest) as! NSBatchDeleteResult
-    
-    let objectIDs = result.result as! [NSManagedObjectID]
-    
-    print("#deleted \(objectIDs.count)")
-    
-    let changes: [AnyHashable: Any] = [NSDeletedObjectsKey: objectIDs]
-    
-    NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [context])
-}
-
-
-func backupData( to FileName:String, from context:NSManagedObjectContext ) throws {
-    
-    
-}
-
-
 
 //
 // MARK: DISCONNECTED KEYENTITY OBJECT
@@ -145,8 +97,8 @@ class KeyItem : ObservableObject, Codable {
         self.expire         = entity.expire
         self.url            = entity.url
 
-        if let data = AppKeychain.shared.getPassword(key: entity.mnemonic) {
-            self.note = data.comment ?? ""
+        if let data = try? UIApplication.shared.getSecrets(key: entity.mnemonic) {
+            self.note = data.note ?? ""
             self.password = data.password
         }
         else {
@@ -263,7 +215,7 @@ class KeyItem : ObservableObject, Codable {
         entity.mnemonic     = self.mnemonic
         entity.username     = self.username
         entity.mail         = self.mail
-        entity.note         = self.note
+        //entity.note       = self.note
         entity.groupPrefix  = self.groupPrefix
         entity.group = NSNumber( value: group )
         entity.expire       = self.expire
@@ -272,7 +224,9 @@ class KeyItem : ObservableObject, Codable {
         return entity
     }
 
-    func insert( into context:NSManagedObjectContext ) {
+    func insert( into context:NSManagedObjectContext ) throws {
+        
+        try UIApplication.shared.setSecrets( key: self.mnemonic, password:self.password, note:self.note)
         
         if let entity = self.entity {
             let _ = self.copyTo(entity: entity )
@@ -282,23 +236,166 @@ class KeyItem : ObservableObject, Codable {
             context.insert( self.copyTo(entity: newEntity) )
         }
 
-        AppKeychain.shared.setPassword(     key: self.mnemonic,
-                                       password: self.password,
-                                        comment: self.note)
-        
     }
     
 }
 
 
-extension SceneDelegate {
+extension UIApplication {
     
     var  managedObjectContext:NSManagedObjectContext {
-        guard let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {
+        guard let context = (delegate as? AppDelegate)?.persistentContainer.viewContext else {
             fatalError("Unable to read managed object context.")
         }
         return context
     }
+    
+    
+    func managedObjectContextInit() {
+        startObservingManagedObjectContextObjectsDidChangeEvent()
+    }
+    
+    func managedObjectContextDestroy() {
+        
+        saveContext()
+        stopObservingManagegObjectContextObjectsDidChangeEvent()
+    }
+
+    private func startObservingManagedObjectContextObjectsDidChangeEvent() {
+        // Add Observer
+        let objectContextObjectsDidChangeEvent = NSNotification.Name.NSManagedObjectContextObjectsDidChange
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(managedObjectContextObjectsDidChange),
+                                               name: objectContextObjectsDidChangeEvent,
+                                               object: managedObjectContext)
+     
+//        notificationCenter.addObserver(self, selector: #selector(managedObjectContextWillSave), name: NSManagedObjectContextWillSaveNotification, object: managedObjectContext)
+//        notificationCenter.addObserver(self, selector: #selector(managedObjectContextDidSave), name: NSManagedObjectContextDidSaveNotification, object: managedObjectContext)
+    }
+
+    private func stopObservingManagegObjectContextObjectsDidChangeEvent() {
+            // Add Observer
+            let objectContextObjectsDidChangeEvent = NSNotification.Name.NSManagedObjectContextObjectsDidChange
+            
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(managedObjectContextObjectsDidChange),
+                                                   name: objectContextObjectsDidChangeEvent,
+                                                   object: managedObjectContext)
+         
+    //        notificationCenter.addObserver(self, selector: #selector(managedObjectContextWillSave), name: NSManagedObjectContextWillSaveNotification, object: managedObjectContext)
+    //        notificationCenter.addObserver(self, selector: #selector(managedObjectContextDidSave), name: NSManagedObjectContextDidSaveNotification, object: managedObjectContext)
+    }
+
+    @objc private func managedObjectContextObjectsDidChange(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+
+        if let inserts = userInfo[NSInsertedObjectsKey] as? Set<NSManagedObject>, inserts.count > 0 {
+            print( "object inserted # \(inserts.count)" )
+        }
+
+        if let updates = userInfo[NSUpdatedObjectsKey] as? Set<NSManagedObject>, updates.count > 0 {
+            print( "object updated # \(updates.count)" )
+        }
+
+        if let deletes = userInfo[NSDeletedObjectsKey] as? Set<NSManagedObject>, deletes.count > 0 {
+            print( "object deleted # \(deletes.count)" )
+            
+            for object in deletes {
+                
+                guard let keyDeleted = object as? KeyEntity else {
+                    print( "obejct \(object) is not a KeyEntity")
+                    continue
+                }
+                
+                do {
+                    try UIApplication.shared.removeSecrets(key: keyDeleted.mnemonic )
+                    print( "secrets \(keyDeleted.mnemonic) removed!")
+                }
+                catch {
+                    print( "error removing password from entity \(keyDeleted)\n\(error)" )
+                }
+            }
+        }
+    }
+        
+    
+    // Core Data Saving support
+    private func saveContext () {
+        let context = managedObjectContext
+
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+    /**
+        Fetch Single Value
+     */
+    func fetchSingle( entity:NSEntityDescription, predicateFormat:String, key:String  ) throws -> Any {
+        let context = managedObjectContext
+
+        let request = NSFetchRequest<NSFetchRequestResult>()
+        request.entity =  entity
+        request.predicate = NSPredicate( format: predicateFormat, key)
+        let fetchResult = try context.fetch( request )
+        
+        if( fetchResult.count == 0 ) {
+            throw SavingError.KeyDoesNotExist(id: key)
+            
+        }
+        if( fetchResult.count > 1 ) {
+            throw SavingError.DuplicateKey(id: key)
+            
+        }
+        return fetchResult[0]
+    }
+
+//    func deleteAll() throws -> Int {
+//        let context = managedObjectContext
+//
+//        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: KeyEntity.fetchRequest())
+//
+//        batchDeleteRequest.resultType = .resultTypeCount
+//
+//        let result = try context.execute(batchDeleteRequest) as! NSBatchDeleteResult
+//
+//        return result.result as! Int
+//    }
+
+    //
+    // @see https://www.avanderlee.com/swift/nsbatchdeleterequest-core-data/
+    //
+    func deleteAllWithMerge() throws {
+        let context = managedObjectContext
+
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: KeyEntity.fetchRequest())
+        
+        batchDeleteRequest.resultType = .resultTypeObjectIDs
+
+        let result = try context.execute(batchDeleteRequest) as! NSBatchDeleteResult
+        
+        let objectIDs = result.result as! [NSManagedObjectID]
+        
+        let changes: [AnyHashable: Any] = [NSDeletedObjectsKey: objectIDs]
+        
+        NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [context])
+    }
+
+    func backupData( to FileName:String ) throws {
+        
+        
+    }
+
+
+
 
 }
 
