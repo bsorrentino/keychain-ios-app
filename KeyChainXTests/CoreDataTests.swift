552e1c7b01fa84cc3ecf71daf2def1429e2b41c2
bsorrentino <bartolomeo.sorrentino@gmail.com>
2020-08-06 15:24:34 +0200

    add unit test for core data

---
+++ KeyChainXTests/CoreDataTests.swift
@@ -0,0 +1,123 @@
+//
+//  CoreDataTests.swift
+//  KeyChainXTests
+//
+//  Created by softphone on 06/08/2020.
+//  Copyright © 2020 Bartolomeo Sorrentino. All rights reserved.
+//
+
+import XCTest
+import CoreData
+@testable import KeychainX
+
+class CoreDataTests: XCTestCase {
+    
+    var container:NSPersistentCloudKitContainer? {
+        var result:NSPersistentCloudKitContainer? = nil
+        
+        let container = NSPersistentCloudKitContainer(name: "KeyChain")
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                
+                let error = "Unresolved error \(error), \(error.userInfo)"
+                print( error )
+                result = nil
+            }
+            else {
+                container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+                result = container
+            }
+            
+
+        }
+        return result
+    }
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        
+        guard let context = self.container?.viewContext else {
+            XCTFail()
+            return
+        }
+        
+        
+        do {
+            let k = NSEntityDescription.insertNewObject(  forEntityName: "KeyInfo", into: context) as! KeyEntity
+
+            //let k = KeyEntity( context:context,  )
+            k.mnemonic = "A1"
+            k.group = 0
+            k.username = "Me"
+            context.insert(k)
+            
+            try context.save()
+        }
+        catch let error as NSError {
+            XCTFail("fail inserting KeyEntity. Error: \(error.userInfo)" )
+        }
+
+
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
+        guard let context = self.container?.viewContext else {
+            XCTFail()
+            return
+        }
+        
+        do {
+            let request:NSFetchRequest<KeyEntity> = KeyEntity.fetchRequest()
+            let result = try context.fetch( request )
+
+            result.forEach { k in context.delete(k) }
+            
+            try context.save()
+        }
+        catch let error as NSError {
+            XCTFail("fail fetching KeyEntity. Error: \(error.userInfo)" )
+        }
+
+    }
+
+    func testCoreData() {
+        
+        guard let context = self.container?.viewContext else {
+            XCTFail()
+            return
+        }
+        
+        do {
+
+            let request:NSFetchRequest<KeyEntity> = KeyEntity.fetchRequest()
+            let result = try context.fetch( request )
+
+            
+            result.forEach { (k) in
+             
+                print( "mnemonic: \(k.mnemonic)")
+            }
+            
+        }
+        catch let error as NSError {
+            XCTFail("fail fetching KeyEntity error \(error.userInfo)" )
+        }
+
+
+    }
+
+}
