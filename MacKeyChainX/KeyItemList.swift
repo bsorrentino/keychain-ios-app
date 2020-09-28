78535c55adfcc4610e63af105764f2f19aa81547
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-09-28 20:20:04 +0200

    update

---
+++ MacKeyChainX/KeyItemList.swift
@@ -0,0 +1,21 @@
+//
+//  KeyItemList.swift
+//  KeyChainX
+//
+//  Created by Bartolomeo Sorrentino on 28/09/20.
+//  Copyright © 2020 Bartolomeo Sorrentino. All rights reserved.
+//
+
+import SwiftUI
+
+struct KeyItemList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KeyItemList_Previews: PreviewProvider {
+    static var previews: some View {
+        KeyItemList()
+    }
+}
---
+++ MacKeyChainX/KeyItemRow.swift
@@ -0,0 +1,21 @@
+//
+//  KeyItemRow.swift
+//  KeyChainX
+//
+//  Created by Bartolomeo Sorrentino on 28/09/20.
+//  Copyright © 2020 Bartolomeo Sorrentino. All rights reserved.
+//
+
+import SwiftUI
+
+struct KeyItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KeyItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        KeyItemRow()
+    }
+}
