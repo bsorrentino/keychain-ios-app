2f19fe304fffa6cc3dc61f1eecd9517d3642f948
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-01-04 18:58:54 +0100

    issue #39 - avoid to type url protocol in text field

---
+++ KeyChainX/String+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  String+Extension.swift
+//  KeyChainX
+//
+//  Created by Bartolomeo Sorrentino on 04/01/21.
+//  Copyright © 2021 Bartolomeo Sorrentino. All rights reserved.
+//
+
+import Foundation
