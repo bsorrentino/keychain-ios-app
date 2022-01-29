8ce331c620bb36e315a8a04cb317307f3d1375f2
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-01-29 19:04:59 +0100

    SwiftUI: add "grouped items" management

---
+++ KeyChainX/DynamicFetchRequestView+KeyEntity.swift
@@ -0,0 +1,9 @@
+//
+//  DynamicFetchRequestView+KeyEntity.swift
+//  KeyChainX
+//
+//  Created by softphone on 29/01/22.
+//  Copyright © 2022 Bartolomeo Sorrentino. All rights reserved.
+//
+
+import Foundation
---
+++ KeyChainX/GroupKeyItemList+IOS15.swift
@@ -0,0 +1,21 @@
+//
+//  GroupKeyItemList+IOS15.swift
+//  KeyChainX
+//
+//  Created by softphone on 29/01/22.
+//  Copyright © 2022 Bartolomeo Sorrentino. All rights reserved.
+//
+
+import SwiftUI
+
+struct GroupKeyItemList_IOS15: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroupKeyItemList_IOS15_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupKeyItemList_IOS15()
+    }
+}
